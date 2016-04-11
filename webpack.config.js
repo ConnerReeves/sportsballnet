a1b8899376dfaf9edc53fa1068c5792970bd77d8
pairing <pairing@randymarsh.f4tech.com>
2016-04-11 12:26:53 -0600

    Remove more hot-loader stuff

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,6 @@ module.exports = {
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'react-hot'
-    },{
-      test: /\.js$/,
-      exclude: /node_modules/,
       loader: 'babel',
       query: {
         presets: ['es2015', 'react']
