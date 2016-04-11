bda17138986eb3e5f41d34a760f526fac5b7ced9
pairing <pairing@randymarsh.f4tech.com>
2016-04-11 12:24:26 -0600

    Removing webpack-dev-server dependency & code

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,12 @@ var webpack = require('webpack');
 
 module.exports = {
   devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './client/index'
-  ],
+  entry: ['./client/index'],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
   module: {
     loaders: [{
       test: /\.js$/,
