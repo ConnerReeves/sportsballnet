87dde82fab1997dd1ab82a85cb8fa281fb5bff25
pairing <pairing@randymarsh.f4tech.com>
2016-04-12 09:09:50 -0600

    wip login

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
+import { Input, ButtonInput } from 'react-bootstrap';
 require('../styles/login.scss');
 
 export default class Login extends Component {
   render() {
     return (
       <div className="login-wrapper">
-        <form className="login-form" action="/login" method="post">
-          <div>
-            <label>Email:</label>
-            <input type="text" name="username" className="form-input" /><br/>
-          </div>
-          <div>
-            <label>Password:</label>
-            <input type="password" name="password" className="form-input" />
-          </div>
-            <div>
-            <input type="submit" value="Submit"/>
-          </div>
+        <form className="login-form" onSubmit={ this._onFormSubmit.bind(this) }>
+          <Input
+            width={ 200 }
+            type="email"
+            label="Email"
+            ref="username"
+          />
+          <Input
+            width={ 200 }
+            type="password"
+            label="Password"
+            ref="password"
+          />
+          <ButtonInput
+            className="login-button"
+            type="submit"
+            value="Login"
+          />
         </form>
       </div>
     );
+  }
+
+  _onFormSubmit() {
+    this.props.login({
+      username: this.refs.username.getValue(),
+      password: this.refs.username.getValue()
+    });
   }
 }
