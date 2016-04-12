51ea7c8ecfd18f4079b057f6804cb2e133fa9155
pairing <pairing@randymarsh.f4tech.com>
2016-04-12 08:28:04 -0600

    prettier login form

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
+require('../styles/login.scss');
 
 export default class Login extends Component {
   render() {
     return (
-      <form action="/login" method="post">
-        <div>
-          <label>Username:</label>
-          <input type="text" name="username"/><br/>
-        </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" name="password"/>
-        </div>
+      <div className="login-wrapper">
+        <form className="login-form" action="/login" method="post">
           <div>
-          <input type="submit" value="Submit"/>
-        </div>
-      </form>
+            <label>Email:</label>
+            <input type="text" name="username" className="form-input" /><br/>
+          </div>
+          <div>
+            <label>Password:</label>
+            <input type="password" name="password" className="form-input" />
+          </div>
+            <div>
+            <input type="submit" value="Submit"/>
+          </div>
+        </form>
+      </div>
     );
   }
 }
