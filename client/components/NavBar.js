0e0d934920f582f3bbabdffd650baa7bcd53c2b8
Conner Reeves <ConnerReeves@gmail.com>
2016-04-12 20:10:37 -0600

    removing gravatar for now

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import UserDisplayContainer from '../containers/UserDisplayContainer';
 require('../styles/nav-bar.scss');
 
 export default class NavBar extends Component {
@@ -10,9 +9,6 @@ export default class NavBar extends Component {
         <Link to="" className="app-logo" />
         <div className="route-links">
           <Link to="leagues">Leagues</Link>
-        </div>
-        <div className="user-display">
-          <UserDisplayContainer />
         </div>
       </div>
     );
