e3365f865e8b8991da288f17837bed2bc8e44e3e
pairing <pairing@randymarsh.f4tech.com>
2016-04-15 14:49:24 -0600

    remove api auth

--- server/api-routes/index.js
+++ server/api-routes/index.js
@@ -5,14 +5,6 @@ const sportRoutes = require('./Sport');
 const userRoutes = require('./User');
 
 module.exports = (app) => {
-  app.use('/api/*', function(req, res, next) {
-    if (process.env.DEV || (req.isAuthenticated && req.isAuthenticated())) {
-      return next();
-    }
-
-    return res.sendStatus(401);
-  });
-
   app.use('/api/games', gameRoutes);
   app.use('/api/leagues', leagueRoutes);
   app.use('/api/organizations', organizationRoutes);
