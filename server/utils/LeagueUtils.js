b8723126af90b55f88de0349131ecb0bb0f7e107
ConnerReeves <ConnerReeves@gmail.com>
2016-04-19 10:23:14 -0600

    updating rating system to be aligned with foosnet

--- server/utils/LeagueUtils.js
+++ server/utils/LeagueUtils.js
@@ -1,70 +1,75 @@
 const Immutable = require('immutable');
 const elo = require('elo-rank')();
 
-const DEFAULT_ELO = 1000;
+const DEFAULT_ELO = 1100;
 
-function getNewElo(playerElo, opponentElos, isWinner) {
-  var adjustedElos = [];
+const getNewElos = (winners, losers) => {
+  var newElos = {};
+  const totalWinnerElo = winners.reduce((sum, winner) => sum + winner.elo, 0);
+  const totalLoserElo = winners.reduce((sum, loser) => sum + loser.elo, 0);
 
-  opponentElos.forEach((opponentElo) => {
-    const expectedScore = elo.getExpected(playerElo, opponentElo);
-    adjustedElos.push(elo.updateRating(expectedScore, isWinner ? 1 : 0, playerElo));
+  const expectedWinnerElo = elo.getExpected(totalWinnerElo, totalLoserElo);
+  const winnersAdjustedElo = elo.updateRating(expectedWinnerElo, 1, totalWinnerElo);
+  winners.forEach((winner) => {
+    const gainRatio = (totalWinnerElo - winner.elo) / totalWinnerElo;
+    const totalGain = winnersAdjustedElo - totalWinnerElo;
+    newElos[winner.playerId] = winner.elo + (totalGain * gainRatio);
   });
 
-  return adjustedElos.reduce((sum, elo) => sum + elo, 0) / adjustedElos.length;
-}
+  const expectedLoserElo = elo.getExpected(totalLoserElo, totalWinnerElo);
+  const losersAdjustedElo = elo.updateRating(expectedLoserElo, 0, totalLoserElo);
+  losers.forEach((loser) => {
+    const gainRatio = (totalLoserElo - loser.elo) / totalLoserElo;
+    const totalGain = losersAdjustedElo - totalLoserElo;
+    newElos[loser.playerId] = loser.elo + (totalGain * gainRatio);
+  });
+
+  return newElos;
+};
 
 module.exports = {
   getPlayerDetails: (games, playerIds) => {
-    var currentElo, opponentElos;
     const playerDetailsMap = games.reduce((playerDetailsMap, game) => {
-      const winners = game.winners.map((id) => id.toString());
-      const losers = game.losers.map((id) => id.toString());
-      const newElos = {};
-
-      winners.forEach((winnerId) => {
-        currentElo = playerDetailsMap.getIn([winnerId, 'elo']) || DEFAULT_ELO;
-        opponentElos = losers.map((loserId) => playerDetailsMap.getIn([loserId, 'elo']) || DEFAULT_ELO);
-        newElos[winnerId] = getNewElo(currentElo, opponentElos, true);
+      const winners = game.winners.map((id) => {
+        const playerId = id.toString();
+        return { playerId, elo: playerDetailsMap.getIn([playerId, 'elo'], DEFAULT_ELO) };
       });
 
-      losers.forEach((loserId) => {
-        currentElo = playerDetailsMap.getIn([loserId, 'elo']) || DEFAULT_ELO;
-        opponentElos = winners.map((winnerId) => playerDetailsMap.getIn([winnerId, 'elo']) || DEFAULT_ELO);
-        newElos[loserId] = getNewElo(currentElo, opponentElos, false);
+      const losers = game.losers.map((id) => {
+        const playerId = id.toString();
+        return { playerId, elo: playerDetailsMap.getIn([playerId, 'elo'], DEFAULT_ELO) };
       });
 
-      winners.forEach((winnerId) => {
-        playerDetailsMap = playerDetailsMap.updateIn([winnerId, 'elo'], (elo) => newElos[winnerId] || DEFAULT_ELO)
-                                           .updateIn([winnerId, 'wins'], (wins) => (wins || 0) + 1)
-                                           .updateIn([winnerId, 'losses'], (losses) => losses || 0)
-                                           .updateIn([winnerId, 'streak'], (streak) => streak > 0 ? streak + 1 : 1)
-                                           .updateIn([winnerId, 'lastPlayed'], (lastPlayed) => game.playedDate);
+      const newElos = getNewElos(winners, losers);
+
+      winners.forEach((winner) => {
+        const playerId = winner.playerId;
+        playerDetailsMap = playerDetailsMap.setIn([playerId, 'elo'], newElos[playerId])
+                                           .setIn([playerId, 'lastPlayed'], (lastPlayed) => game.playedDate.toString())
+                                           .updateIn([playerId, 'wins'], (wins) => (wins || 0) + 1)
+                                           .updateIn([playerId, 'streak'], (streak) => streak > 0 ? streak + 1 : 1);
       });
 
-      losers.forEach((loserId) => {
-        playerDetailsMap = playerDetailsMap.updateIn([loserId, 'elo'], (elo) => newElos[loserId] || DEFAULT_ELO)
-                                           .updateIn([loserId, 'losses'], (losses) => (losses || 0) + 1)
-                                           .updateIn([loserId, 'wins'], (wins) => wins || 0)
-                                           .updateIn([loserId, 'streak'], (streak) => streak < 0 ? streak - 1 : -1)
-                                           .updateIn([loserId, 'lastPlayed'], (lastPlayed) => game.playedDate);
+      losers.forEach((loser) => {
+        const playerId = loser.playerId;
+        playerDetailsMap = playerDetailsMap.setIn([playerId, 'elo'], newElos[playerId])
+                                           .setIn([playerId, 'lastPlayed'], (lastPlayed) => game.playedDate.toString())
+                                           .updateIn([playerId, 'losses'], (losses) => (losses || 0) + 1)
+                                           .updateIn([playerId, 'streak'], (streak) => streak < 0 ? streak - 1 : -1);
       });
 
       return playerDetailsMap;
     }, Immutable.Map()).toJS();
 
-    playerIds.forEach((playerId) => {
-      playerId = playerId.toString();
-
-      if (!playerDetailsMap[playerId]) {
-        playerDetailsMap[playerId] = {
-          elo: DEFAULT_ELO,
-          lastPlayed: null,
-          losses: 0,
-          streak: 0,
-          wins: 0
-        };
-      }
+    playerIds.forEach((id) => {
+      const playerId = id.toString();
+      playerDetailsMap[playerId] = Object.assign({
+        elo: DEFAULT_ELO,
+        lastPlayed: null,
+        losses: 0,
+        streak: 0,
+        wins: 0
+      }, playerDetailsMap[playerId]);
 
       playerDetailsMap[playerId].elo = Math.round(playerDetailsMap[playerId].elo);
     });
