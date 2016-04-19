70a7052203087c3efd9d5fed6379c59b6315db32
ConnerReeves <ConnerReeves@gmail.com>
2016-04-19 14:32:58 -0600

    fix elos for single player team games

--- server/utils/LeagueUtils.js
+++ server/utils/LeagueUtils.js
@@ -7,22 +7,27 @@ const getNewElos = (winners, losers) => {
   var newElos = {};
   const totalWinnerElo = winners.reduce((sum, winner) => sum + winner.elo, 0);
   const totalLoserElo = winners.reduce((sum, loser) => sum + loser.elo, 0);
-
   const expectedWinnerElo = elo.getExpected(totalWinnerElo, totalLoserElo);
-  const winnersAdjustedElo = elo.updateRating(expectedWinnerElo, 1, totalWinnerElo);
-  winners.forEach((winner) => {
-    const gainRatio = (totalWinnerElo - winner.elo) / totalWinnerElo;
-    const totalGain = winnersAdjustedElo - totalWinnerElo;
-    newElos[winner.playerId] = winner.elo + (totalGain * gainRatio);
-  });
-
   const expectedLoserElo = elo.getExpected(totalLoserElo, totalWinnerElo);
+  const winnersAdjustedElo = elo.updateRating(expectedWinnerElo, 1, totalWinnerElo);
   const losersAdjustedElo = elo.updateRating(expectedLoserElo, 0, totalLoserElo);
-  losers.forEach((loser) => {
-    const gainRatio = (totalLoserElo - loser.elo) / totalLoserElo;
-    const totalGain = losersAdjustedElo - totalLoserElo;
-    newElos[loser.playerId] = loser.elo + (totalGain * gainRatio);
-  });
+
+  if (winners.length === 1 && losers.length === 1) {
+    newElos[winners[0]] = winnersAdjustedElo;
+    newElos[losers[0]] = losersAdjustedElo;
+  } else {
+    winners.forEach((winner) => {
+      const gainRatio = (totalWinnerElo - winner.elo) / totalWinnerElo;
+      const totalGain = winnersAdjustedElo - totalWinnerElo;
+      newElos[winner.playerId] = winner.elo + (totalGain * gainRatio);
+    });
+
+    losers.forEach((loser) => {
+      const gainRatio = (totalLoserElo - loser.elo) / totalLoserElo;
+      const totalGain = losersAdjustedElo - totalLoserElo;
+      newElos[loser.playerId] = loser.elo + (totalGain * gainRatio);
+    });
+  }
 
   return newElos;
 };
